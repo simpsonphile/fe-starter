3187325cb1b43d83cb974b416c4d268824c8a135
Dawid Mazewski <d.mazewski@iamcodee.co>
2019-06-05 10:25:29 +0200

    Add sass compiler, autoprefixer, css minification, sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 const gulp = require("gulp")
+
 const webpack = require('webpack')
 const webpackConfig = require('./webpack.config.js')
+
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+const cleanCSS = require('gulp-clean-css')
+const sourcemaps = require('gulp-sourcemaps')
 
 function assets(cb) {
   return new Promise((resolve, reject) => {
@@ -16,4 +22,23 @@ function assets(cb) {
 })
 }
 
-exports.build = gulp.series(assets)
+gulp.task('buildStyles', () => {
+  return gulp.src('./src/sass/**/*.scss')
+  .pipe(sass.sync().on('error', sass.logError))
+  .pipe(autoprefixer({
+    overrideBrowserslist: ['last 2 versions'],
+    cascade: false
+}))
+  .pipe(gulp.dest('./dist/css/'))
+})
+
+gulp.task('minifyStyles', () => {
+  return gulp.src('./dist/css/*.css')
+  .pipe(sourcemaps.init())
+  .pipe(cleanCSS({compatibility: 'ie8'}))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('./dist/css/'))
+})
+
+gulp.task('build', gulp.series(assets, 'buildStyles'))
+gulp.task('production', gulp.series(assets, 'buildStyles', 'minifyStyles'))
