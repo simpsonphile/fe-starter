ca4e439fddbdea182d6e618be410617868ad1206
Dawid Mazewski <d.mazewski@iamcodee.co>
2019-06-05 10:27:17 +0200

    Fix js dist path

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   // Path and filename of your result bundle.
   // Webpack will bundle all JavaScript into this file
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist/js'),
     filename: 'main.js'
   },
 
